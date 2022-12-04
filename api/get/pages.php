8ef41ac46c8631a5e4d13eb6cf7ed028958f8a0a
Onnwen <onnwen.cassitto@icloud.com>
2022-12-04 20:23:28 +0100

    Implementata gestione pagine

---
+++ api/get/pages.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../connection.php';
+
+if (!isset($_SESSION["user_id"])) {
+    var_dump(http_response_code(401));
+    exit();
+}
+
+$sql = "SELECT * FROM pages";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+exit();
