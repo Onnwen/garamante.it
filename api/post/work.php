a638fec07ab35f79510c7c71b6b0d88646e0c44d
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 14:55:43 +0100

    Correzione percorso

--- api/post/work.php
+++ api/post/work.php
@@ -8,11 +8,11 @@ $repository = $_POST['repository'] ?? '';
 $typeId = $_POST['typeId'] ?? '';
 $cover = $_FILES["cover"];
 
-$target_dir_cover = "contents/portfolio/covers/";
+$target_dir_cover = "/var/www/html/Garamante-Website/contents/portfolio/covers/";
 
-$target_file_cover = $target_dir_cover . $title;
+$target_file_cover = $target_dir_cover . $cover["name"];
 
-move_uploaded_file($cover["tmp_name"], $target_file_cover . ".jpg");
+move_uploaded_file($cover["tmp_name"], $target_file_cover);
 
 $Query = "INSERT INTO Garamante.works (title, description_preview, description, type_id, cover_image_id, repository_url) VALUES (:title, :descriptionPreview, :description, :typeId, :coverImageUrl, :repositoryUrl);";
 try {
