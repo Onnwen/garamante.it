daf19836bd6dfe4e689a6d633b143667f5a4133f
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 17:33:04 +0100

    Risolti bug minori

--- api/get/works.php
+++ api/get/works.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../connection.php';
 
-$sql = "SELECT works.id, title, description_preview, images.url as cover_image_url, works_type.name as type, repository_url
+$sql = "SELECT works.id, title, description_preview, description, images.url as cover_image_url, works_type.name as type, repository_url
 FROM works
          INNER JOIN works_type ON works.type_id = works_type.id
          INNER JOIN images ON works.cover_image_id = images.id";
