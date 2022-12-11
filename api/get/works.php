29fc4881930243c6adeb601b657834c1d2424fb9
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 16:01:19 +0100

    Risolto bug

--- api/get/works.php
+++ api/get/works.php
@@ -3,7 +3,7 @@ require_once '../connection.php';
 
 $sql = "SELECT works.id, title, description_preview, cover_image_url, works_type.name as type, repository_url
 FROM works
-         INNER JOIN works_type ON works.type_id = works_type.id;
+         INNER JOIN works_type ON works.type_id = works_type.id
          INNER JOIN images ON works.cover_image_id = images.id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
