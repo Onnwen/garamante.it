e34662faf31bf8f6742e7626c1d7b38acb13d95c
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 16:05:46 +0100

    Risoluzione bug vari

--- api/get/works.php
+++ api/get/works.php
@@ -1,12 +1,18 @@
 <?php
 require_once '../connection.php';
 
-$sql = "SELECT works.id, title, description_preview, cover_image_url, works_type.name as type, repository_url
+$sql = "SELECT works.id, title, description_preview, images.url as cover_image_url, works_type.name as type, repository_url
 FROM works
          INNER JOIN works_type ON works.type_id = works_type.id
          INNER JOIN images ON works.cover_image_id = images.id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
+
+$works = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($works as $key => $work) {
+    $works[$key]['description_preview'] = urldecode($work['description_preview']);
+    $works[$key]['description'] = urldecode($work['description']);
+}
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 exit();
