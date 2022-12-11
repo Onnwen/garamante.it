33a9d23516c892b817847ea8d4b5a4c79445637a
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 15:53:50 +0100

    Risoluzione bug

--- api/post/work.php
+++ api/post/work.php
@@ -15,11 +15,11 @@ $target_file_cover = $target_dir_cover . $cover["name"];
 
 move_uploaded_file($cover["tmp_name"], $target_file_cover);
 $path = substr($target_file_cover, 31);
-echo addslashes($path);
+echo quotemeta($path);
 
 try {
     $pdo->beginTransaction();
-    $pdo->exec("INSERT INTO images (alias_text, url) VALUES (" . $title . "," . addslashes($path) . ")");
+    $pdo->exec("INSERT INTO images (alias_text, url) VALUES (" . $title . "," . quotemeta($path) . ")");
     $pdo->exec("INSERT INTO works (title, description, description_preview, repository_url, type_id, cover_image_id) VALUES (" . $title .", " . $description . ", " .$descriptionPreview . ", " . $repository . ", " .$typeId . ", LAST_INSERT_ID())");
     $pdo->commit();
 } catch (Exception $e) {
