0a1bb082505d7a84dfcaf502ab321d012b2f7698
Onnwen <onnwen.cassitto@icloud.com>
2022-12-11 15:10:28 +0100

    debug

--- api/post/work.php
+++ api/post/work.php
@@ -1,36 +1,35 @@
 <?php
 require_once('../connection.php');
 
-echo exec('whoami');
-//
-//$title = $_POST['title'] ?? '';
-//$description = $_POST['description'] ?? '';
-//$descriptionPreview = $_POST['descriptionPreview'] ?? '';
-//$repository = $_POST['repository'] ?? '';
-//$typeId = $_POST['typeId'] ?? '';
-//$cover = $_FILES["cover"];
-//
-//$target_dir_cover = "/var/www/html/Garamante-Website/contents/portfolio/covers/";
-//
-//$target_file_cover = $target_dir_cover . $cover["name"];
-//
-//move_uploaded_file($cover["tmp_name"], $target_file_cover);
-//
-//$Query = "INSERT INTO Garamante.works (title, description_preview, description, type_id, cover_image_id, repository_url) VALUES (:title, :descriptionPreview, :description, :typeId, :coverImageUrl, :repositoryUrl);";
-//try {
-//    $pre = $pdo->prepare($Query);
-//} catch (Exception $e) {
-//    echo $e->getMessage();
-//    exit;
-//}
-//
-//$pre->bindParam(':title', $title);
-//$pre->bindParam(':descriptionPreview', $descriptionPreview);
-//$pre->bindParam(':description', $description);
-//$pre->bindParam(':typeId', $typeId);
-//$pre->bindParam(':coverImageUrl', $target_file_cover);
-//$pre->bindParam(':repositoryUrl', $repository);
-//
-//$pre->execute();
-//
-//exit;
+
+$title = $_POST['title'] ?? '';
+$description = $_POST['description'] ?? '';
+$descriptionPreview = $_POST['descriptionPreview'] ?? '';
+$repository = $_POST['repository'] ?? '';
+$typeId = $_POST['typeId'] ?? '';
+$cover = $_FILES["cover"];
+
+$target_dir_cover = "/var/www/html/Garamante-Website/contents/portfolio/covers/";
+
+$target_file_cover = $target_dir_cover . $cover["name"];
+
+move_uploaded_file($cover["tmp_name"], $target_file_cover);
+
+$Query = "INSERT INTO Garamante.works (title, description_preview, description, type_id, cover_image_id, repository_url) VALUES (:title, :descriptionPreview, :description, :typeId, :coverImageUrl, :repositoryUrl);";
+try {
+    $pre = $pdo->prepare($Query);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+$pre->bindParam(':title', $title);
+$pre->bindParam(':descriptionPreview', $descriptionPreview);
+$pre->bindParam(':description', $description);
+$pre->bindParam(':typeId', $typeId);
+$pre->bindParam(':coverImageUrl', $target_file_cover);
+$pre->bindParam(':repositoryUrl', $repository);
+
+$pre->execute();
+
+exit;
