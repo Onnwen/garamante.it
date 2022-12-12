96d71d80c9a2b059e5d30ff87bd77a50cf0c1286
Onnwen <onnwen.cassitto@icloud.com>
2022-12-12 19:01:01 +0100

    Risoluzione bug

--- api/pull.php
+++ api/pull.php
@@ -1,16 +1,10 @@
 <?php
 require_once('connection.php');
-//shell_exec('git pull');
+shell_exec('git pull');
 
 $user_id = $_POST['user_id'] ?? '';
 
-try {
-    $sql = "INSERT INTO Garamante.chronology (user_id, event_id, date, content_name) VALUES (:userId, 1, DEFAULT, 'garamante.it')";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':userId', $user_id);
-    $stmt->execute();
-}
-catch (Exception $e) {
-    echo $e->getMessage();
-    exit;
-}
+$sql = "INSERT INTO Garamante.chronology (user_id, event_id, date, content_name) VALUES (:userId, 1, DEFAULT, 'garamante.it')";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':userId', $user_id);
+$stmt->execute();
