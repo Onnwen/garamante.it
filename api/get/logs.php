88809bdb39739852068af45ff8ec85d39efa3d98
Onnwen <onnwen.cassitto@icloud.com>
2022-12-12 19:44:03 +0100

    Risoluzione bug

--- api/get/logs.php
+++ api/get/logs.php
@@ -9,7 +9,7 @@ require_once '../connection.php';
 $sql = "SELECT CONCAT(users.first_name, ' ', users.last_name) as user, date, content_name, name as event
 FROM chronology
          INNER JOIN chronology_events ON chronology.event_id = chronology_events.id
-         INNER JOIN users ON chronology.user_id = users.id ORDER BY date DESC LIMIT 10";
+         INNER JOIN users ON chronology.user_id = users.id ORDER BY date DESC LIMIT 5";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 
