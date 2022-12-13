acc0a6b8d980006f6b1ba4ec4c9109a8fc2aa0a3
Onnwen <onnwen.cassitto@icloud.com>
2022-12-13 14:17:06 +0100

    Update stringUtility.js

--- js/stringUtility.js
+++ js/stringUtility.js
@@ -1,11 +1,3 @@
-function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
-
-function replaceAll(str, find, replace) {
-    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-}
-
 function parseLogs(logs, regex) {
     let isFirst = true;
     return logs.replaceAll(regex, function (date) {
