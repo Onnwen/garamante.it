cbee75a44526aff6f5e75946f8e427c5f5bb48ae
Onnwen <onnwen.cassitto@icloud.com>
2022-12-13 14:10:36 +0100

    Ottimizzazione codice

--- js/stringUtility.js
+++ js/stringUtility.js
@@ -5,3 +5,16 @@ function escapeRegExp(string) {
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
+
+function parseLogs(logs, regex) {
+    let isFirst = true;
+    return logs.replaceAll(regex, function (date) {
+        if (isFirst) {
+            isFirst = false;
+            return date;
+        } else {
+            isFirst = true;
+            return "<hr>" + date;
+        }
+    });
+}
