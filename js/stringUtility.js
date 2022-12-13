f860f6f555195e99ee1c01d47ce5c816ab0ae2a3
Onnwen <onnwen.cassitto@icloud.com>
2022-12-13 14:03:31 +0100

    Test

---
+++ js/stringUtility.js
@@ -0,0 +1,7 @@
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
